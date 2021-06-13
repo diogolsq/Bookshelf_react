93285264f7278cc4a5cf78730111558b29ecfc20
diogolsq <diogolsq@poli.ufrj.br>
2021-06-13 10:21:38 -0300

    escrevendo o call to action

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -5,17 +5,17 @@ export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
-        <h1 className="display-4">Food Recipes</h1>
+        <h1 className="display-4">Gerenciador de Leitura</h1>
         <p className="lead">
-          A curated list of recipes for the best homemade meal and delicacies.
+          Ferramenta usando preceitos de metodologia ágil, agora servindo você para gerenciar seus livros
         </p>
         <hr className="my-4" />
         <Link
-          to="/recipes"
+          to="/kanban"
           className="btn btn-lg custom-button"
           role="button"
         >
-          View Recipes
+          Comece a mapear sua leitura agora
         </Link>
       </div>
     </div>
