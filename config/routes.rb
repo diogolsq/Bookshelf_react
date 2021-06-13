0d4b5b9088bcbb91575c5ee0705380a584528ee7
diogolsq <diogolsq@poli.ufrj.br>
2021-06-13 13:38:07 -0300

    começando a fazer o user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'users/index'
+      get 'users/create'
+      get 'users/show'
+      get 'users/destroy'
+    end
+  end
   devise_for :users
   root 'homepage#index' 
 end
