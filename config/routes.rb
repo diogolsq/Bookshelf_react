897bb75a645b3df8d822d65d7213c7a11f5e82bd
diogolsq <diogolsq@poli.ufrj.br>
2021-06-13 13:46:15 -0300

    consertando erro no route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'users/index'
       get 'users/create'
       get 'users/show'
-      get 'users/destroy'
     end
   end
   devise_for :users
   root 'homepage#index' 
+  get '/*path' => 'homepage#index'
 end
