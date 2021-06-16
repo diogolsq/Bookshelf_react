f182d2ac287b5f414b75a9c824512acf0fbb6b8a
diogolsq <diogolsq@poli.ufrj.br>
2021-06-16 07:47:23 -0300

    fazendo um rescu de erro caso o save quebre no meio

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -30,29 +30,34 @@ class ShelvesController < ApplicationController
 
    
     def save
-        data_hash = params[:shelves].to_unsafe_h
-        livros_estante = data_hash[:estante].map{|livro| livro["id"]}
-        livros_lendo = data_hash[:lendo].map{|livro| livro["id"]}
-        livros_lido = data_hash[:lido].map{|livro| livro["id"]}
+        status = 200
+        begin
+            data_hash = params[:shelves].to_unsafe_h
+            livros_estante = data_hash[:estante].map{|livro| livro["id"]}
+            livros_lendo = data_hash[:lendo].map{|livro| livro["id"]}
+            livros_lido = data_hash[:lido].map{|livro| livro["id"]}
 
-        user_id = params[:user][:user].to_i
+            user_id = params[:user][:user].to_i
 
-        livros_estante.each do |livro_id|
-            shelf = Shelf.where(book_id: livro_id, user_id: user_id).first
-            shelf.update_attributes(status: 'estante') if shelf
+            livros_estante.each do |livro_id|
+                shelf = Shelf.where(book_id: livro_id, user_id: user_id).first
+                shelf.update_attributes(status: 'estante') if shelf
+            end
+
+            livros_lendo.each do |livro_id|
+                shelf = Shelf.where(book_id: livro_id, user_id: user_id).first
+                shelf.update_attributes(status: 'lendo') if shelf
+            end
+
+            livros_lido.each do |livro_id|
+                #byebug
+                shelf = Shelf.where(book_id: livro_id, user_id: user_id).first
+                shelf.update_attributes(status: 'lido') if shelf
+            end
+        rescue
+            status = 500
         end
-
-        livros_lendo.each do |livro_id|
-            shelf = Shelf.where(book_id: livro_id, user_id: user_id).first
-            shelf.update_attributes(status: 'lendo') if shelf
-        end
-
-        livros_lido.each do |livro_id|
-            #byebug
-            shelf = Shelf.where(book_id: livro_id, user_id: user_id).first
-            shelf.update_attributes(status: 'lido') if shelf
-        end
-
+        render json: {status: status}
     end
 
 
